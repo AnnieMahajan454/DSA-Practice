c850f2ac2233940991952b4b4a29b682010b0554
Annie Mahajan <142834998+AnnieMahajan454@users.noreply.github.com>
2025-09-23 09:36:40 +0530

    Create MergeIntervals.java

---
+++ Array/MergeIntervals.java
@@ -0,0 +1,10 @@
+//given an array of intervals, intervals[i]=[starti,endi]
+//merge all overlapping intervals and return an array of non overlapping intervals
+
+//brute force approach
+import java.util.*;
+class Solution{
+  public int[][] merge(int[][] intervals){
+    
+  }
+}
