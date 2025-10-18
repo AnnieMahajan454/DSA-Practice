b26fc0e9f40edfffb92b82f3325bdbc730abf473
Annie Mahajan <142834998+AnnieMahajan454@users.noreply.github.com>
2025-10-18 22:59:56 +0530

    Add twoSum method for finding indices of two numbers
    Implemented the twoSum method using a HashMap for O(n) time complexity.

--- Array/Week1/Day1 - Array Basics + Prefix Sum/TwoSum.java
+++ Array/Week1/Day1 - Array Basics + Prefix Sum/TwoSum.java
@@ -13,3 +13,19 @@ class Solution {
 }
 
 //Time complexity O(n^2)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+
+//Time complexity O(n)
