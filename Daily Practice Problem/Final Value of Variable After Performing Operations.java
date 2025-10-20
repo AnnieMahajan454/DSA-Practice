5a85b2cb16d2b3efa7333a0b0315fe77b549a24b
Annie Mahajan <142834998+AnnieMahajan454@users.noreply.github.com>
2025-10-20 22:36:05 +0530

    Add solution for final value of variable after operations
    Implement finalValueAfterOperations method to calculate the final value of x based on given operations.

---
+++ Daily Practice Problem/Final Value of Variable After Performing Operations.java
@@ -0,0 +1,15 @@
+//Time complexity: O(n)
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(String op: operations){
+            if(op.contains("++")){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+        return x;
+    }
+}
