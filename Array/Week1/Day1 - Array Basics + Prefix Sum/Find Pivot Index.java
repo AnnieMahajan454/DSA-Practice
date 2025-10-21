b34890dd5b85bf7872f7081d48bcd64d48597135
Annie Mahajan <142834998+AnnieMahajan454@users.noreply.github.com>
2025-10-21 15:19:44 +0530

    Add pivot index method to find the pivot index
    Implement pivot index calculation in the Solution class.

---
+++ Array/Week1/Day1 - Array Basics + Prefix Sum/Find Pivot Index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (2 * leftSum + nums[i] == totalSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
+
+//Time complexity = O(n)
